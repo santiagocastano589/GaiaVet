fdce427d77716147056acb2c47e49d02ce06b283
Juan Jose Gomez <juanjosegomez182@gmail.com>
2024-06-06 17:29:56 -0500

    finish the design of the header

--- src/Components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Button = ({textButton}) => {
-  return (
-    <>
-        <button className='p-3 my-4 rounded-lg bg-blue-border w-60'>{textButton}</button>
-    </>
-  )
-}
--- src/Components/Layouts/Header/Header.jsx
+++ src/Components/Layouts/Header/Header.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import './Header.css'
 import logo from '../../../assets/logoGaia.webp'
+import menu from '../../..//assets/Iconos/MenuHamburguesa.svg'
 
 
 export const Header = () => {
   return (
-    <header className='w-full'>
+    <header className='w-full fixed'>
 
-      <div className='ola h-48 flex items-center justify-evenly'>
+      <div className='ola w-full h-48 flex items-center justify-between object-cover'>
 
-        <div className='w-20 bg-white rounded-full flex items-center'>
-          <img className='' src={logo} alt="" />
-          <h2 className=''>GaiaVet</h2>
+        <div className='flex items-center justify-center w-1/3'>
+          <div className=' bg-white w-20 h-20 rounded-full flex items-center justify-center'>
+
+            <img className='w-16 h-16 cursor-pointer' src={logo} alt="" />
+          </div>
+          <h2 className='text-3xl cursor-pointer'>GaiaVet</h2>
         </div>
 
-        <h2 class="text-8xl gorditas text-black text-stroke-2-white">GaiaVet</h2>
+        <div className='w-1/3 flex justify-center'>
+          <h2 class="text-9xl gorditas text-black text-stroke-2-white fixed top-20">GaiaVet</h2>
+        </div>
+
+        <div className='w-1/3 flex justify-center'>
+          <img src={menu} alt="" className='cursor-pointer' />
+        </div>
 
       </div>
 
