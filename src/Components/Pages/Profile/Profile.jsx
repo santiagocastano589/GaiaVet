1b1150604c6b028574589f6fde67cacc94196365
Daniel Osorio <ndmd997@gmail.com>
2024-07-22 15:00:16 -0500

    se hacen cambios en el perfil

--- src/Components/Pages/Profile/Profile.jsx
+++ src/Components/Pages/Profile/Profile.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Header } from '../../Layouts/Header/Header'
 import ImgUser from '../../../assets/perfil.webp'
 import './Profile.css'
 import { InputProfile } from '../../InputProfile/InputProfile'
 
 export const Profile = () => {
+  
   return (
     <>
         <Header/>
@@ -17,26 +18,28 @@ export const Profile = () => {
 
                 <div className='flex flex-col items-center justify-center w-full'>
                     
-                        <img className='rounded-full w-72 h-72 my-5' src={ImgUser} alt="" />
-                        <p className='text-5xl mb-5'>Pepito Perez</p>
+                        <img className='rounded-full w-80 h-80 my-5' src={ImgUser} alt="" />
+                        <p className='text-5xl mb-5'>Jeronimo Arias Mosquera</p>
                 </div>
 
                 <div className='w-full flex flex-col justify-center items-center   '>
                     
-                    <div className='w-2/4 h-96  bg-blue-border rounded-xl p-8'>
-                        <InputProfile lblName='Nombre' InValue='Pepito Perez' />
-                        <InputProfile lblName='Correo' InValue='Pepito Perez' />
-                        <InputProfile lblName='Dirección' InValue='Pepito Perez' />
-                        <InputProfile lblName='Telefono' InValue='Pepito Perez' />
-                        <button className='px-5 py-1 bg-black text-white rounded-lg float-end me-16'>Editar</button>
+                    <div className='w-2/4 h-  bg-blue-border rounded-xl p-8'>
+
+                        <InputProfile lblName='Nombre' initialValue='Jeronimo' />
+                        <InputProfile lblName='Apellido' initialValue='Arias Mosquera' />
+                        <InputProfile lblName='Correo' initialValue='jeroarias@gmail.com' />
+                        <InputProfile lblName='Dirección' initialValue='corbones calle 17 #23-57 casa #7' />
+                        <InputProfile lblName='Telefono' initialValue='3022606983' />
+                        <button className='px-5 py-1 bg-black text-white rounded-lg float-end me-16 hover:bg-gray-600'>Editar</button>
                     </div>
                 </div>
             </div>
 
             <div className='flex justify-center'>
             <div className='w-3/5 flex justify-evenly my-10  '>
-                <button className='w-60 h-12 rounded-xl bg-buttonProducts text-white' type="button">Cerrar Sesión</button>
-                <button className='w-60 h-12 rounded-xl bg-red-600 text-white' type="button">Eliminar cuenta</button>
+                <button className='w-60 h-12 rounded-xl hover:bg-emerald-900 bg-buttonProducts text-white' type="button">Cerrar Sesión</button>
+                <button className='w-60 h-12 rounded-xl hover:bg-red-400 hover:text-black bg-red-600 text-white' type="button">Eliminar cuenta</button>
             </div>
             </div>
            
