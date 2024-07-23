dfc3ff1ed943bdf09c58d9be033f341ca09b12c8
jhonjurado <jhonedisonjurado57@gmail.com>
2024-07-23 13:30:37 -0500

    finish the xl query

--- src/Components/CartServices/CartServices.jsx
+++ src/Components/CartServices/CartServices.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const CartServices = ({ service, image, alt }) => {
   return (
-    <div className='w-52 flex flex-col items-center hover:-translate-y-2 hover: duration-500 hover: cursor-pointer sm:mx-[6rem] sm:my-[2rem] md:my-[2rem] xl:mx-7'>
+    <div className='w-52 flex flex-col items-center hover:-translate-y-2 hover: duration-500 hover: cursor-pointer sm:mx-[6rem] sm:my-[2rem] md:my-[2rem] xl:mx-7 lg:my-0'>
         <div className='p-4 px-8 border-2 border-black rounded-t-3xl border-b-0'>
             <img src={image} alt={alt}/>
         </div>
