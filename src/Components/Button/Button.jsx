3769d440b82f5a0b500cf132dffbab4444beb1cd
Daniel Osorio <ndmd997@gmail.com>
2024-08-01 17:35:15 -0500

    se agrega el apartado de reseña

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -4,8 +4,8 @@ export const Button = ({ textButton, onClick }) => {
   return (
     <button
       type='button'
-      onClick={onClick} // Corrección aquí
-      className='text-white hover:bg-teal-300 shadow-lg shadow-gray-500/50 p-3 w-72 my-4 rounded-lg bg-blue-border'
+      onClick={onClick} 
+      className='text-white hover:bg-teal-400 shadow-lg shadow-gray-500/50 p-3 w-72 my-4 rounded-lg bg-blue-border'
     >
       {textButton}
     </button>
