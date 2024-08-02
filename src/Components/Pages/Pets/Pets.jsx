3274954e95f28f9b989fd35d503ac3d0a0fa0527
Juan Jose Gomez <juanjosegomez182@gmail.com>
2024-08-02 16:53:48 -0500

    se agrega la funcion de listar mascotas

--- src/Components/Pages/Pets/Pets.jsx
+++ src/Components/Pages/Pets/Pets.jsx
@@ -1,40 +1,73 @@
-import React,{useEffect, useState} from 'react'
-import { Header } from '../../Layouts/Header/Header'
-import { ContainerPets } from '../../ContainerPets/ContainerPets'
-
-
+import React, { useEffect, useState, useContext } from 'react';
+import { Header } from '../../Layouts/Header/Header';
+import { ContainerPets } from '../../ContainerPets/ContainerPets';
+import { AuthContext } from '../../Context/Context';
 
 export const Pets = () => {
+  const [petList, setPetList] = useState([]);
 
-  // https://gaiavet-back.onrender.com/auth/login'
+  const { authToken } = useContext(AuthContext);
 
-  // useEffect(() => {
-  
-  //   const [petList,setPetList] = useState([])
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const response = await fetch('https://gaiavet-back.onrender.com/Pets', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
 
-  //   return () => {
-      
-  //   }
-  // })
+        const data = await response.json();
+        
+        // Verificar que la respuesta sea un array
+        if (Array.isArray(data)) {
+          setPetList(data);
+        } else {
+          console.error('La respuesta no es un array:', data);
+        }
+      } catch (error) {
+        console.log('Error al traer las mascotas:', error);
+      }
+    };
+
+    fetchPets();
+  }, [authToken]);
 
   return (
     <>
-    <Header title="Busca tu mascota"/>
-
-    <div className=''>
-
-    <div className='flex justify-center pt-48'>
-    <div className='flex flex-row items-center w-2/4'>
-      <input type="text" placeholder='Ingresa tu documento o nombre de tu mascota' className='flex-grow rounded-md border border-gray-300 p-2' />
-      <button type="button" class='ml-2 bg-blue-border hover:bg-teal-300 hover:text-black text-white font-bold py-2 px-4 rounded-md'>Buscar</button>
-    </div> 
-    </div>
-
-    <ContainerPets namePet="FIRULAIS" documento="1212331" tipo="perro" raza="criollo"/>
-    <ContainerPets namePet="JUAN" documento="33333" tipo="perro" raza="criollo"/>
-    <ContainerPets namePet="ALEX" documento="888" tipo="perro" raza="criollo"/>
-
-    </div>
+      <Header title="Busca tu mascota" />
+      <div className="">
+        <div className="flex justify-center pt-48">
+          <div className="flex flex-row items-center w-2/4">
+            <input
+              type="text"
+              placeholder="Ingresa tu documento o nombre de tu mascota"
+              className="flex-grow rounded-md border border-gray-300 p-2"
+            />
+            <button
+              type="button"
+              className="ml-2 bg-blue-border hover:bg-teal-300 hover:text-black text-white font-bold py-2 px-4 rounded-md"
+            >
+              Buscar
+            </button>
+          </div>
+        </div>
+        {Array.isArray(petList) ? (
+          petList.map((pet) => (
+            <ContainerPets
+              key={pet.idMascota}
+              namePet={pet.nombre}
+              documento={pet.idMascota}
+              tipo={pet.edad}
+              raza={pet.raza}
+            />
+          ))
+        ) : (
+          <p>No se encontraron mascotas</p>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
