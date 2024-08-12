56ffaba76fe5740f34b554e8d0a8b7e10a67f91b
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-12 16:41:08 -0500

    I upload new products registration x

--- src/Components/Pages/AdminProducts/AdminProducts.jsx
+++ src/Components/Pages/AdminProducts/AdminProducts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import { AuthContext } from '../../Context/Context';
 import { Header } from '../../Layouts/Header/Header';
 import ProductRegisterModal from '../../ProductRegisterModal/ProductRegisterModal';
+import img1 from '../../../assets/comidaGato.png'
 
 export const AdminProducts = () => {
   const [productsList, setProductsList] = useState([]);
@@ -52,19 +53,22 @@ export const AdminProducts = () => {
     <>
       <Header title="Lista de Productos" />
       <div className="flex justify-center pt-48">
-        <div className="w-full flex justify-center my-10">
+        <div className="w-full flex justify-center my-10 overflow-hidden">
           <div className="w-[90%] h-[20rem] p-6 mb-[20rem]">
-            <div className="flex justify-end mb-4">
+            <div className="flex items-center justify-between mb-4 bg-teal-200 h-[8rem] px-4 rounded-xl">
+              <h3 className=''>Holaa!!  Presiona el boton para registrar un nuevo producto</h3>
               <button 
-                className="bg-teal-500 text-white py-2 px-4 rounded hover:bg-teal-400"
+                className="bg-teal-500 text-white py-2 px-4 rounded h-[3rem] hover:bg-teal-400"
                 onClick={openModal}
               >
                 Registrar Producto
               </button>
             </div>
+            <div className='overflow-y-auto max-h-[30rem]'>
             <table className="w-full bg-white border-4">
               <thead>
                 <tr className="w-full bg-teal-500 text-gray-800 uppercase text-sm">
+                  <th className="py-3 px-6 text-center">Imagen del Producto</th>
                   <th className="py-3 px-6 text-center">Nombre Producto</th>
                   <th className="py-3 px-6 text-center">Categoria</th>
                   <th className="py-3 px-6 text-center">Descripcion</th>
@@ -75,6 +79,7 @@ export const AdminProducts = () => {
               <tbody className="text-gray-600 text-sm">
                 {productsList.map((product) => (
                   <tr key={product.idProducto} className="border-b border-gray-200 hover:bg-gray-100">
+                    <td className="py-3 px-6 text-center"><img src={img1} alt="" /></td>
                     <td className="py-3 px-6 text-center">{product.nombreProducto}</td>
                     <td className="py-3 px-6 text-center">{product.categoria}</td>
                     <td className="py-3 px-6 text-center">{product.descripcion}</td>
@@ -84,6 +89,9 @@ export const AdminProducts = () => {
                 ))}
               </tbody>
             </table>
+
+            </div>
+            
           </div>
         </div>
       </div>
