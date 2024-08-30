ab225ae70d03fc4458755455eee03b5137505d4b
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-30 14:18:38 -0500

    I upload changes products shop

--- src/Components/ContainerCategory/ContainerCategory.jsx
+++ src/Components/ContainerCategory/ContainerCategory.jsx
@@ -3,21 +3,29 @@ import { CategoryCard } from '../CategoryCard/CategoryCard'
 import tapetes from "../../assets/tapetes-extra-gruesos.webp";
 import comidaPerro from "../../assets/comidaPerro.png";
 import comidaGato from "../../assets/comidaGato.png";
+import gaiavet from "../../assets/alimentos-para-mascotas.png";
+import gaiavet2 from "../../assets/huesos.png";
+import gaiavet3 from "../../assets/comida-de-gato.png";
+import gaiavet4 from "../../assets/champu-para-mascotas.png";
 
 export const ContainerCategory = ({setSelectedCategory}) => {
   return (
     <>
         <div className='w-full pt-[10rem]'>
             <div className='w-full flex '>
+            <div onClick={() => setSelectedCategory('')}>
+                <CategoryCard image={gaiavet} title={'Todos los productos'} alt={'producto'}/>
+              </div>
               <div onClick={() => setSelectedCategory('Comida para perros')}>
-                <CategoryCard image={comidaPerro}  title={'Comida para perros'} alt={'producto'}/>
+                <CategoryCard image={gaiavet2}  title={'Comida para perros'} alt={'producto'}/>
               </div>
               <div onClick={() => setSelectedCategory('Comida para gatos')}>
-                <CategoryCard image={comidaGato}  title={'Comida para gatos'} alt={'producto'}/>
+                <CategoryCard image={gaiavet3}  title={'Comida para gatos'} alt={'producto'}/>
               </div>
-              <div onClick={() => setSelectedCategory('Aseo para tu mascota')}>
-                <CategoryCard image={tapetes} title={'Aseo para tu mascota'} alt={'producto'}/>
+              <div onClick={() => setSelectedCategory('Aseo')}>
+                <CategoryCard image={gaiavet4} title={'Aseo para tu mascota'} alt={'producto'}/>
               </div>
+
             </div>
         </div>
         
